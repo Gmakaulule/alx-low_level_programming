d61ec290a2c7a0ddc960dc2346c3d52740323129
Gmakaulule <giftmakaulule@gmail.com>
2022-06-06 03:54:48 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int z = '\n';
+	int x = 'a';
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+	if (x == 'q' || x == 'e')
+		continue;
+	 putchar(x);
+	}
+	putchar(z);
+
+	return (67);
+}
