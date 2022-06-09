22eba87afcfbe049f8fee87e17b100dafa0c5cb6
Gmakaulule <giftmakaulule@gmail.com>
2022-06-09 15:28:17 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_numbers - prints numbers from 0 to 9
+* Return:0
+*/
+
+	void print_most_numbers(void)
+	{
+	char x = 0;
+
+	while (x <= 9) && (x != 2) && (x !=4)
+	{
+		_putchar(x + '0');
+		x++;
+
+	}
+		_putchar('\n');
+
+}
