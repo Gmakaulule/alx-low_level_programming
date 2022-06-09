9e9ebc67129a1f2e05b0e151188a80d81e6bfb1f
Gmakaulule <giftmakaulule@gmail.com>
2022-06-09 08:48:57 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -17,19 +17,18 @@ int _isupper(int c)
 	{
 		_putchar(c);
 		_putchar(':');
-		_putchar('1');
+		return ('1');
 
 	}
 		else if (c >= 97 || c <= 122)
 		{
 		_putchar(c);
 		_putchar(':');
-		_putchar('0');
+		return (0);
 		
 
 		}
 
 	_putchar('\n');
 
-	return (0);
-}
+	return (0); }
