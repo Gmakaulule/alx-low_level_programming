bc1d3a8f1a26224be8d7a000a8c4f1e4a819386f
Gmakaulule <giftmakaulule@gmail.com>
2022-06-10 03:49:47 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * void more_numbers(void);
+ *
+ *
+ */
+
+
+void more_numbers(void)
+
+	
+{	
+	int x = 0;
+	
+	while (x >= 0) (x <= 9)
+
+	{	for (x >=0, x<= 13, x++)
+		{
+			_putchar(x);
+		}
+		x++;
+	
+	}
+	
+	_putchar('\n')
+
+		return (0)
+
+}
