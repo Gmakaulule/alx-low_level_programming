cd75bd8c7b521f8e90f6d9852451d1d0d148c272
Gmakaulule <giftmakaulule@gmail.com>
2022-06-13 17:51:04 -0700

    9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copystring
+ *@src: sourse string
+ * Return: returning string
+ *@dest: destination string
+ */
+
+char *_strcpy(char *dest, char *src)
+
+{
+	char *x = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+		return (x);
+}
