68d964eb25f2b7c4c282086db040f0844a73f70b
Gmakaulule <giftmakaulule@gmail.com>
2022-06-16 11:22:04 -0700

    6-cap

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -31,6 +31,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *_strncat(char *dest, char *src, int n);
+char *cap_string(char *);
 
 #endif /* MAIN_H */
 
