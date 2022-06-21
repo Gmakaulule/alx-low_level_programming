2997d021df6457ddf04b072fbbca69156e5b6707
Gmakaulule <giftmakaulule@gmail.com>
2022-06-21 17:04:40 +0000

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@
 
 int _isdigit(int c);
 int _putchar(char c);
-int _isupper(int c);
+int _isupper(int *c);
 int mul(int a, int b);
 void print_most_numbers(void);
 void print_line(int n);
@@ -36,5 +36,5 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
-
+void print_chessboard(char (*a)[8]);
 #endif /* MAIN_H */
