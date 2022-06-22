111e61ccc8edf7f0b6bdf32aeeb688ad2b8df34b
Gmakaulule <giftmakaulule@gmail.com>
2022-06-22 16:42:19 +0000

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _puts_recursion - print string
+ * @s: string to be printed
+ * return: void
+ */
+
+void _puts_recursion(char *s)
+{
+
+
+	if (*s == '\0')
+	{
+		return;
+	}
+	{
+	_putchar(*s);
+		s++;
+	_puts_recursion(s);
+	_putchar('\n');
+	}
+}
