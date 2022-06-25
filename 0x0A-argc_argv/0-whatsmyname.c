3f3dfa12b2a0adf47d4e0431203880543abe3684
Gmakaulule <giftmakaulule@gmail.com>
2022-06-25 11:55:15 +0000

    bugfix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,10 @@
  *
  * Return: Alwayss zero (0)
  */
-int main(int argc, char *argv[])
+
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	(void) argc;
-	printf("%s/n", argv[0];
+	printf("%s\n", argv[0]);
+
 	return (0);
 }
