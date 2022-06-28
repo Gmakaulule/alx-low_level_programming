07f068ade7314b625df12eb4e5b590c27aaa9ab6
Gmakaulule <giftmakaulule@gmail.com>
2022-06-28 14:28:26 +0000

    grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free two dimetional grid
+ * @grid : input two dimettinal array
+ * @height: hight length 
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
