819a26cb2117a61425c647e4043e9095631a41a3
Gmakaulule <giftmakaulule@gmail.com>
2022-07-04 20:40:10 +0000

    debug

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdio.h>
 /**
  * init_dog - initialise structure variable
  * @d: pointer to structure
@@ -10,7 +9,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
