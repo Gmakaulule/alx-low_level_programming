044282f1e1b1d583cf2947fc48a782adda05c524
Gmakaulule <giftmakaulule@gmail.com>
2022-07-06 14:58:03 +0000

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints name
+ * @name: string to be printed
+ * @f: pointer to function print-name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL && name != NULL)
+		f(name);
+}
