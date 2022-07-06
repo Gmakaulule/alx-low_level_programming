92e6e22b759d9b25eae10187d4a69b36e6f5ab94
Gmakaulule <giftmakaulule@gmail.com>
2022-07-06 17:50:29 +0000

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,8 +12,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 {
 
-	size_t i;
+	size_t i = 0;
 
+    if(array != NULL && size > 0 && action != NULL)
 	for (i = 0 ; i < size; i++)
 	{
 	action(array[i]);
