43a96e6532818319dcd82dee9f27598ec794c90d
Gmakaulule <giftmakaulule@gmail.com>
2022-07-19 18:49:46 +0000

    debug

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,6 +11,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_head = (listint_t *)malloc(sizeof(listint_t));
 
+	if (new_head == NULL)
+	{
+		return (NULL);
+	}
 	new_head->n =  n;
 	new_head->next = *head;
 	*head = new_head;
