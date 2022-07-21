c8f3a684e854082ebaed4d4667551254b96e2892
Gmakaulule <giftmakaulule@gmail.com>
2022-07-21 21:57:34 +0000

    4-clear_bit.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,3 +4,4 @@ int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
