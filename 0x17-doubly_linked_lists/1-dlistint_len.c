1b7ce6fea5ec52f0f4cd523360592ee685b2f2e7
Gmakaulule <giftmakaulule@gmail.com>
2022-09-01 12:52:18 +0000

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+   const dlistint_t *temp;
+   size_t count = 0;
+
+    if(h != NULL)
+    {
+        for(temp = h;temp != NULL;temp = temp->next)
+        {
+            count++;
+        }
+    }
+        return(count);
+}
