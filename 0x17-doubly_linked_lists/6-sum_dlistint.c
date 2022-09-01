74ad08f12d7fbda604a7af239fbe280a6d14e96d
Gmakaulule <giftmakaulule@gmail.com>
2022-09-01 18:46:08 +0000

    6-sum_dlistint.c

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * sum_dlistint - add all numbers in a linked list
+  * @head: pointer to the first node of doubly linked list
+  *
+  * Return: The sum of list
+  */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+	int sum = 0;
+
+	if (head)
+	{
+		while (current != NULL)
+		{
+			sum += temp->n;
+			temp = temp->next;
+		}
+	}
+
+	return (sum);
+}
