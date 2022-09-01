f9a715f252d82e204c45d005455c998d476c0710
Gmakaulule <giftmakaulule@gmail.com>
2022-09-01 12:48:40 +0000

    debug

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,11 +7,7 @@ size_t print_dlistint(const dlistint_t *h)
    const dlistint_t *temp;
    size_t count = 0;
 
-    if(h == NULL)
-    {
-        printf("list is empty");
-    }
-    else
+    if(h != NULL)
     {
         for(temp = h;temp != NULL;temp = temp->next)
         {
